10ecd521c37b05652412edaa50954f5699fde1c1
yesmaberg-stack <yesmaberg@gmail.com>
2025-12-17 05:17:22 -0500

    Create index.js
    Initial Next.js home page added to fix Vercel deployment.

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <div style={{
+      minHeight: '100vh',
+      background: '#0b0b12',
+      color: 'white',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      fontFamily: 'Arial'
+    }}>
+      <h1>VISIONTrading</h1>
+    </div>
+  );
+}
