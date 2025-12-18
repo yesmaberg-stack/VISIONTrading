dfdc5b5d7da955cd67de1df28faedbabdef2484a
yesmaberg-stack <yesmaberg@gmail.com>
2025-12-18 17:56:59 -0500

    Add login page

---
+++ login.js
@@ -0,0 +1,15 @@
+export default function Login() {
+  return (
+    <div style={{
+      minHeight: '100vh',
+      background: '#0b0b12',
+      color: 'white',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center'
+    }}>
+      <h1>VISIONTrading Login</h1>
+    </div>
+  )
+}
+
